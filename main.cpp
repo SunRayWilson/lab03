14cf9d318895ad1c6272efd6bce1350e5ca4b3ef
Nadia (PotylikoNA) <u211-13@public.mpei.local>
2019-04-01 11:10:10 +0300

    build: часть 1

--- main.cpp
+++ main.cpp
@@ -27,39 +27,29 @@ find_minmax(vector<double> numbers, double& min, double& max) {
     }
 
 }
-int
-main() {
-    // Ввод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
 
-    cerr << "Enter numbers: ";
-
-   // const vector<double> numbers = input_numbers(number_count);
-
-   const auto numbers = input_numbers(number_count);
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-
-   // Обработка данных
-    double min,max;
+void
+   make_histogram (vector<double> numbers, size_t count, vector<size_t>& bins ){
+      double min,max;
     find_minmax(numbers, min, max);
 
 
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
+  for (double number : numbers) {
+        size_t bin = ((number - min) / (max - min) * count);
+        if (bin == count) {
             bin--;
         }
         bins[bin]++;
     }
+  }
 
-    // Вывод данных
-    const size_t SCREEN_WIDTH = 80;
+
+
+
+void
+show_histogram_text( vector<size_t>& bins )
+{
+ const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
     size_t max_count = 0;
@@ -70,7 +60,7 @@ main() {
     }
     const bool scaling_needed = max_count > MAX_ASTERISK;
 
-    for (size_t bin : bins) {
+for (size_t bin : bins) {
         if (bin < 100) {
             cout << ' ';
         }
@@ -90,6 +80,32 @@ main() {
         }
         cout << '\n';
     }
+}
+
+
+int
+main() {
+    // Ввод данных
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    //const vector<double>& numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(number_count);
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+   // Обработка данных
+
+   // vector<size_t> bins(bin_count);
+     vector<size_t> bins(bin_count);
+     make_histogram(numbers, bin_count, bins);
+
+    //const size_t bins = make_histogram(numbers, bin_count);
+    show_histogram_text(bins);
 
     return 0;
 }
